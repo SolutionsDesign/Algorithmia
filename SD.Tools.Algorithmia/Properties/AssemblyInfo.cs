d0e183626412a19746e2f45f99c93e491ee3246e
Frans Bouma <frans@sd.nl>
2018-02-09 15:53:31 +0100

    Clean up, reference of bclextensions 1.2.1

--- SD.Tools.Algorithmia/Properties/AssemblyInfo.cs
+++ SD.Tools.Algorithmia/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
 [assembly: AssemblyFileVersion("1.3.18.0209")]
